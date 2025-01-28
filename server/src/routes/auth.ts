8716c5289d4798608c1af1ab2f6316dd785f05a5
Kjorgy <164044713+KJUDALL@users.noreply.github.com>
2025-01-27 21:38:51 -0500

    added personal JWT token in .env file

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,7 +1,7 @@
 //  Created user authentication routes for user registration and Login to handle JWT generation  -AJ
 // filepath: server/src/routes/auth.ts
 import express from 'express';
-import User from '../models/User';
+import User from '../models/user';
 import { generateToken, verifyToken } from '../../utils/jwt';
 
 const router = express.Router();
