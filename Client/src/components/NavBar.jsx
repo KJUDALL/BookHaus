bb37c0a9951e54a8dfcd9239e737edb7a2ca08df
Kjorgy <164044713+KJUDALL@users.noreply.github.com>
2025-01-30 18:23:24 -0500

    update to file path to resolve navbar issue with render deployment

--- Client/src/components/NavBar.jsx
+++ Client/src/components/NavBar.jsx
@@ -1,19 +1,29 @@
-import { Link } from 'react-router-dom';
-import './Navbar.css'; // Make sure to import your CSS file
+import { Link } from "react-router-dom";
+import "./NavBar.css"; // Make sure to import your CSS file
 
 function Navbar() {
-    return (
-        <nav className="navbar">
-            <h1 className="navbar-title">BookHaus</h1>
-            <ul className="navbar-links">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/profile">Your Shelf</Link></li>
-                <li><Link to="/recommendations">Recommendations</Link></li>
-                <li><Link to="/library-feed">Library Feed</Link></li>
-                <li><Link to="/login">Login Page</Link></li>
-            </ul>
-        </nav>
-    );
+	return (
+		<nav className='navbar'>
+			<h1 className='navbar-title'>BookHaus</h1>
+			<ul className='navbar-links'>
+				<li>
+					<Link to='/'>Home</Link>
+				</li>
+				<li>
+					<Link to='/profile'>Your Shelf</Link>
+				</li>
+				<li>
+					<Link to='/recommendations'>Recommendations</Link>
+				</li>
+				<li>
+					<Link to='/library-feed'>Library Feed</Link>
+				</li>
+				<li>
+					<Link to='/login'>Login Page</Link>
+				</li>
+			</ul>
+		</nav>
+	);
 }
 
 export default Navbar;
